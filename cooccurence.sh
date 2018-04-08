0e2eb3492e4983f0c1afc38c039cb19dac81232e
Beautimind <htang6@buffalo.edu>
2018-04-08 16:19:50 -0400

    add files

---
+++ cooccurence.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+hadoop jar /home/hadoop/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.6.4.jar \
+-file /home/hadoop/nytime/comapper.py    -mapper /home/hadoop/nytime/comapper.py \
+-file /home/hadoop/nytime/reducer.py   -reducer /home/hadoop/nytime/reducer.py \
+-input input -output output
